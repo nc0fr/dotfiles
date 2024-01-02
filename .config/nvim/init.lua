3c5ba577a97c53748c95cbac8a1b884419ebae95
Nicolas Paul <n@nc0.fr>
2024-01-02 11:36:30 +0100

    neovim: Init Neovim configuration directory
    Signed-off-by: Nicolas Paul <n@nc0.fr>

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- Copyright 2024 Nicolas Paul.  All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the COPYRIGHT file.
+
+--                         █████
+--                       ███░░░███
+--  ████████    ██████  ███   ░░███     Nicolas Paul
+-- ░░███░░███  ███░░███░███    ░███
+--  ░███ ░███ ░███ ░░░ ░███    ░███     n@nc0.fr
+--  ░███ ░███ ░███  ███░░███   ███      https://x.com/nc0_r
+--  ████ █████░░██████  ░░░█████░
+-- ░░░░ ░░░░░  ░░░░░░     ░░░░░░
+--
+-- Nicolas Paul's Neovim configuration.
+--
+-- The configuration is structured organically, following the concept
+-- of VIM's runtimepath (rpt).
+-- This means that the configuration is separated in different directories
+-- depending on their goals.  See `:help rtp` for more information.
+
