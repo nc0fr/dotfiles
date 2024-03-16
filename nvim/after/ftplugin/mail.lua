ee5abec8e599f6318ee1fe1433b03b741b429da1
Nicolas Paul <n@nc0.fr>
2024-03-16 10:50:19 +0100

    Add few language-specific options for Neovim
    This patch makes Neovim use a specific set of options for some
    languages.  For instance in Go, it tries to follow the commonly
    agreed upon format from "gofmt".
    
    Signed-off-by: Nicolas Paul <n@nc0.fr>

---
+++ nvim/after/ftplugin/mail.lua
@@ -0,0 +1,10 @@
+-- Copyright (c) 2024 Nicolas Paul.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the COPYRIGHT file.
+
+vim.o.colorcolumn = "72"
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.textwidth = 72
