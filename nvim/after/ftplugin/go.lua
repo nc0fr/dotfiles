fc5a1852f71c158c927ecd4e841be8bc10001846
Nicolas Paul <n@nc0.fr>
2024-03-17 16:21:13 +0100

    Reset Neovim configuration
    Signed-off-by: Nicolas Paul <n@nc0.fr>

--- nvim/after/ftplugin/go.lua
+++
@@ -1,7 +0,0 @@
--- Copyright (c) 2024 Nicolas Paul.
--- Use of this source code is governed by a BSD-style
--- license that can be found in the COPYRIGHT file.
-
--- Commonly accepted Go style guide.
-vim.o.colorcolumn = "80"
-vim.o.expandtab = false
--- nvim/init.lua
+++ nvim/init.lua
@@ -71,6 +71,10 @@ vim.opt.guifont = 'MonoLisa Nerd Font:h12'
 -- I do not want to feel lost when I'm using another Neovim instance, e.g.
 -- remote servers.
 
+-- Because I type to quickly
+vim.keymap.set('n', 'W', 'w')
+vim.keymap.set('n', 'Wq', 'wq')
+
 -- Disable arrow keys, I want to use hjkl instead.
 vim.api.nvim_set_keymap('n', '<Up>', '<Nop>',
     { noremap = true, silent = true })
@@ -84,3 +88,32 @@ vim.api.nvim_set_keymap('n', '<Right>', '<Nop>',
 -- See http://vimcasts.org/blog/2013/01/oil-and-vinegar-split-windows-and-project-drawer/
 vim.api.nvim_set_keymap('n', '<Leader>t', ':Explore<CR>',
     { noremap = true, silent = true })
+
+-- Quick way to :nohl
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
+    { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
+    { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float,
+    { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,
+    { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Be a chad
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>',
+    { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>',
+    { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>',
+    { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>',
+    { desc = 'Move focus to the upper window' })
