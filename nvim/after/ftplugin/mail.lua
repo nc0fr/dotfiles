5edf867183eef09773c6ebb1468ff38f4c6c0513
Nicolas Paul <n@nc0.fr>
2024-04-04 19:31:20 +0200

    nvim: Move mail ftplugin to Lua
    Signed-off-by: Nicolas Paul <n@nc0.fr>

--- nvim/after/ftplugin/mail.lua
+++ nvim/after/ftplugin/mail.lua
@@ -2,9 +2,9 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the COPYRIGHT file.
 
-vim.o.colorcolumn = "72"
-vim.opt.expandtab = true
-vim.opt.wrap = false
+vim.o.colorcolumn        = "72"
+vim.opt.expandtab        = true
+vim.opt.wrap             = false
 vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.textwidth = 72
+vim.opt_local.tabstop    = 2
+vim.opt_local.textwidth  = 72
