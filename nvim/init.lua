05db491b67c7982bfe1253b3d8324b3625a9d4be
Nicolas Paul <n@nc0.fr>
2024-04-04 19:12:05 +0200

    nvim: align code
    Signed-off-by: Nicolas Paul <n@nc0.fr>

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,30 +36,32 @@ vim.g.have_nerd_font = false
 --
 -- Also keep in mind that Neovim enables some options by default, reducing the
 -- configuration compared to classical VIM 8.
-vim.opt.autoindent = false
-vim.opt.clipboard = 'unnamed'
-vim.opt.cursorline = true
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.inccommand = 'split'
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.mouse = 'a'
-vim.opt.number = true
+vim.opt.autoindent     = false
+vim.opt.clipboard      = 'unnamed'
+vim.opt.cursorline     = true
+vim.opt.hlsearch       = true
+vim.opt.ignorecase     = true
+vim.opt.inccommand     = 'split'
+vim.opt.list           = true
+vim.opt.listchars      = { tab   = '» '
+                         , trail = '·'
+                         , nbsp  = '␣' }
+vim.opt.mouse          = 'a'
+vim.opt.number         = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 15
-vim.opt.showmode = false
-vim.opt.signcolumn = 'yes'
-vim.opt.smartcase = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.undofile = true
+vim.opt.scrolloff      = 15
+vim.opt.showmode       = false
+vim.opt.signcolumn     = 'yes'
+vim.opt.smartcase      = true
+vim.opt.splitbelow     = true
+vim.opt.splitright     = true
+vim.opt.undofile       = true
 
 -- Set theme,
 -- I use the default theme, but I want to use the light variant when I'm using
 -- my OS in light mode, and the dark variant when I'm using my OS in dark mode.
 vim.opt.termguicolors = true
-vim.opt.background = 'light'
+vim.opt.background    = 'light'
 vim.cmd('colorscheme default')
 
 -- Set the font to use MonoLisa, a font I bought and love.
@@ -72,39 +74,31 @@ vim.opt.guifont = 'MonoLisa Nerd Font:h12'
 -- remote servers.
 
 -- Because I type to quickly
-vim.keymap.set('n', 'W', 'w')
+vim.keymap.set('n', 'W',  'w')
 vim.keymap.set('n', 'Wq', 'wq')
-vim.keymap.set('n', 'U', ':redo')
+vim.keymap.set('n', 'U',  ':redo')
 
--- See http://vimcasts.org/blog/2013/01/oil-and-vinegar-split-windows-and-project-drawer/
-vim.api.nvim_set_keymap('n', '<Leader>t', ':Explore<CR>',
-    { noremap = true, silent = true })
+-- See:
+-- http://vimcasts.org/blog/2013/01/oil-and-vinegar-split-windows-and-project-drawer/
+vim.api.nvim_set_keymap('n', '<Leader>t', ':Explore<CR>')
 
 -- Quick way to :nohl
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
-    { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
-    { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float,
-    { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,
-    { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '[d',        vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d',        vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Be a chad
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<Left>',  '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<Right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<Up>',    '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<Down>',  '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>',
-    { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>',
-    { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>',
-    { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>',
-    { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
