d8ade7a19fa99b15bc63c8dc79d859d91950cc28
Nicolas Paul <n@nc0.fr>
2024-05-06 22:34:34 +0200

    nvim: remove comments to speed up parser
    Signed-off-by: Nicolas Paul <n@nc0.fr>

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,26 +2,6 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the COPYRIGHT file.
 
---
---          .-') _   ('-.                     (`-.           _   .-')
---         ( OO ) )_(  OO)                  _(OO  )_        ( '.( OO )_
---     ,--./ ,--,'(,------. .-'),-----. ,--(_/   ,. \ ,-.-') ,--.   ,--.)
---     |   \ |  |\ |  .---'( OO'  .-.  '\   \   /(__/ |  |OO)|   `.'   |
---     |    \|  | )|  |    /   |  | |  | \   \ /   /  |  |  \|         |
---     |  .     |/(|  '--. \_) |  |\|  |  \   '   /,  |  |(_/|  |'.'|  |
---     |  |\    |  |  .--'   \ |  | |  |   \     /__),|  |_.'|  |   |  |
---     |  | \   |  |  `---.   `'  '-'  '    \   /   (_|  |   |  |   |  |
---     `--'  `--'  `------'     `-----'      `-'      `--'   `--'   `--'
---
---
--- Nicolas Paul's Neovim configuration entry point.  This file is written in
--- Lua as it is the most powerful and recommended way to configure Neovim.
--- I first thought about using Fennel, as I love Lisp, but I think removing an
--- abstraction and build time dependency is a good thing.
---
--- This configuration is scattered in multiple files in various directories,
--- trying to follow the "pseudo-standard" runtime layout of VIM-like editors.
-
 -- Leader key, set before anything else to avoid any registration issues.
 vim.g.mapleader      = ' '
 vim.g.maplocalleader = ' '
@@ -31,11 +11,6 @@ vim.g.maplocalleader = ' '
 -- (source: kickstart.nvim)
 vim.g.have_nerd_font = false
 
--- Editor options
--- Use :h to get more information about each option.
---
--- Also keep in mind that Neovim enables some options by default, reducing the
--- configuration compared to classical VIM 8.
 vim.opt.autoindent     = false
 vim.opt.clipboard      = 'unnamed'
 vim.opt.colorcolumn    = '80'
@@ -60,9 +35,7 @@ vim.opt.splitbelow     = true
 vim.opt.splitright     = true
 vim.opt.undofile       = true
 
--- Set theme,
--- I use the default theme, but I want to use the light variant when I'm using
--- my OS in light mode, and the dark variant when I'm using my OS in dark mode.
+-- TODO(nc0): support background setting given the OS settings.
 vim.opt.termguicolors = true
 vim.opt.background    = 'light'
 vim.cmd('colorscheme default')
@@ -99,9 +72,3 @@ vim.keymap.set('n', '<Left>',  '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<Right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<Up>',    '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<Down>',  '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
